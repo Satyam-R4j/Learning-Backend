1880fcf8155a4b70fb71aef505bdeff879aa1fcb
Satyam-R4j <satyamraj418@gmail.com>
2026-01-10 02:15:47 +0530

    DB connnection

--- Backend-chaiorcode/src/index.js
+++ Backend-chaiorcode/src/index.js
@@ -1,1 +1,15 @@
-import mongoose from "mongoose";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+});
+
+(async () => {
+    try {
+        await connectDB();
+    } catch (error) {
+        console.error("Failed to start application:", error);
+        process.exit(1);
+    }
+})();
