6be98e18412e7cdc72fe040dc39f1ffbf4ced471
Satyam-R4j <satyamraj418@gmail.com>
2026-01-11 02:39:37 +0530

    Working on Api Handling & Error Handling

--- Backend-chaiorcode/src/index.js
+++ Backend-chaiorcode/src/index.js
@@ -1,15 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './.env'
+    path: "./.env",
 });
 
-(async () => {
-    try {
-        await connectDB();
-    } catch (error) {
-        console.error("Failed to start application:", error);
-        process.exit(1);
-    }
-})();
+connectDB()
+    .then(()=>{
+        app.listen(process.env.PORT || 8000, ()=>{
+            console.log(`Server is running on port : ${process.env.PORT}`);
+            
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!!", err);
+    });
